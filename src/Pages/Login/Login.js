5432bf1f69e5e96384e3da1d4f954fda615c38dd
Maisha589 <maisharahman589@gmail.com>
2022-05-26 22:18:11 +0600

    final

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -51,7 +51,7 @@ const Login = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6">Welcome to our <span>ELECTRONIC TOOL ZONE</span>. Here you will have your best manufactured tools used for electronic purpose in your company.</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form onSubmit={handleSubmit(onSubmit)}>
